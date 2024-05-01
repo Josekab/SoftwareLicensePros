e1bf8c6f10e9ed5454db76e2054ca64b8ed67acf
Jose Carlos Barrantes <113412848+Josekab@users.noreply.github.com>
2024-05-01 17:11:57 -0600

    Update

---
+++ README.md
@@ -0,0 +1,24 @@
+##For English 3 proyect
+
+## Setup
+```bash
+$ git clone 
+$ cd shop
+$ npm i
+$ npm start
+```
+
+## Build
+```bash
+$ npm run build
+```
+
+## Test the build
+To test prpl-server build:
+```bash
+$ npm run serve:prpl-server
+```
+To test static build:
+```bash
+$ npm run serve:static
+```
---
+++ src/lazy-resources.js
@@ -0,0 +1,24 @@
+
+
+// shop-app
+import "./shop-icons.js";
+import "./shop-analytics.js";
+import "./shop-cart-data.js";
+import "./shop-cart-modal.js";
+import "./shop-snackbar.js";
+import "./shop-tabs.js";
+import "./shop-tab.js";
+import "@polymer/paper-icon-button/paper-icon-button.js";
+import "@polymer/app-layout/app-drawer/app-drawer.js";
+
+// shop-list
+// shop-detail
+import "./shop-network-warning.js";
+import "./shop-404-warning.js";
+
+// shop-cart
+import "./shop-cart-item.js";
+
+// shop-checkout
+import "@polymer/iron-form/iron-form.js";
+import "@polymer/paper-spinner/paper-spinner-lite.js";
